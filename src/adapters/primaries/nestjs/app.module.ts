b0ae7f08642dc538e4e1c172ee45a6b93d5d4b28
Rayanator <rayan.galetta@hotmail.com>
2023-02-16 16:15:46 +0100

    fixed archi 2

--- src/adapters/primaries/nestjs/app.module.ts
+++ src/adapters/primaries/nestjs/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { QuestionsController } from './questions/questions.controller';
 import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [],
   controllers: [QuestionsController, UsersController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
